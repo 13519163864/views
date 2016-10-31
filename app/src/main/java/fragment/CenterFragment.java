9675819b8a18140baf7da769fa2fb59877119da8
13519163864 <13519163864@163.com>
2016-10-31 12:50:41 +0800

    xlistview

--- app/src/main/java/fragment/CenterFragment.java
+++ app/src/main/java/fragment/CenterFragment.java
@@ -2,6 +2,7 @@ package fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -9,23 +10,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ListView;
 
 import com.zhuoxin.main.views.Activity_ViewsShow;
 import com.zhuoxin.main.views.R;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import adapter.Adapter_Menu;
 import entry.Source;
 import interFace.OnResourceListener;
+import me.maxwin.view.XListView;
 import utils.Task;
 
 /**
  * Created by Administrator on 2016/10/28.
  */
 
-public class CenterFragment extends Fragment implements OnResourceListener, AdapterView.OnItemClickListener {
+public class CenterFragment extends Fragment implements XListView.IXListViewListener, OnResourceListener, AdapterView.OnItemClickListener {
 
     @Nullable
     @Override
@@ -35,16 +38,23 @@ public class CenterFragment extends Fragment implements OnResourceListener, Adap
     }
 
     public static final String PATH = "http://118.244.212.82:9092/newsClient/path/news_list?ver=1&subid=1&dir=1&nid=1&stamp=20140321&cnt=20";
-    ListView mLst;
+    XListView mLst;
     ArrayList<Source> list = new ArrayList<>();
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
         super.onViewCreated( view, savedInstanceState );
-        mLst = (ListView) view.findViewById( R.id.lst_menu );
+        handler = new Handler();
+        mLst = (XListView) view.findViewById( R.id.lst_menu );
         initData();
         mLst.setOnItemClickListener( this );
+        //上拉加载
+        mLst.setPullLoadEnable( true );
+        //下拉刷新
+        mLst.setPullRefreshEnable( true );
+        //设置监听
+        mLst.setXListViewListener( this );
 
     }
 
@@ -60,6 +70,7 @@ public class CenterFragment extends Fragment implements OnResourceListener, Adap
         Log.e( "===", "list" + list.size() );
         Adapter_Menu adapter = new Adapter_Menu( list, getContext() );
         mLst.setAdapter( adapter );
+
     }
 
     @Override
@@ -68,4 +79,34 @@ public class CenterFragment extends Fragment implements OnResourceListener, Adap
         intent.putExtra( "i", i );
         startActivity( intent );
     }
+
+    @Override
+    public void onRefresh() {
+        handler.postDelayed( new Runnable() {
+            @Override
+            public void run() {
+                stop();
+            }
+        }, 2000 );
+    }
+
+    @Override
+    public void onLoadMore() {
+        handler.postDelayed( new Runnable() {
+            @Override
+            public void run() {
+                stop();
+            }
+        }, 2000 );
+    }
+
+    public void stop() {
+        mLst.stopLoadMore();
+        mLst.stopRefresh();
+        SimpleDateFormat format = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss" );
+        String date = format.format( new Date( System.currentTimeMillis() ) );
+        mLst.setRefreshTime( date );
+    }
+
+    Handler handler;
 }
